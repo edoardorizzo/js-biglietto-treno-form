b8fa537a697bb47a01de609f72e77c6f6afaf314
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-09 15:11:34 +0100

    add js function

--- assets/script.js
+++ assets/script.js
@@ -5,30 +5,43 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65. */
 
 // Chiedi all'utente i km da percorrere e l'età
-const km = Number("Quanti chilometri vuoi percorrere?");
-const age = Number("Qual è la tua età?");
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click',function() {
+    let userNameInput = document.getElementById('name');
+    let kmInput = (document.getElementById('km'));
+    let ageInput = document.getElementById('eta');
 
-console.log(km, age);
+    let userName = userNameInput.value;
+    let km = kmInput.value;
+    let age = ageInput.value;
+
+    console.log(userName, km, age);
+})
+
+let userAge = document.getElementById('eta')
+let kmToDo = document.getElementById('km')
+console.log('userAge', 'kmToDo');
+    
 
 // Calcola il prezzo del biglietto
 const pricePerKm = 0.21;
-let totalPrice = km * pricePerKm;
-
+let totalPrice = kmToDo * pricePerKm;
 console.log(totalPrice);
 
 // Applica lo sconto per i minorenni e gli over 65
-if (age < 18) {
+
+ if (userAge == document.getElementById('under')) {
   discount =  0.2; // Sconto del 20%
-} else if (age >= 65) {
+} else if (userAge == document.getElementById('over')) {
   discount = 0.4; // Sconto del 40%
 }
 
-let totalDiscount = totalPrice * discount;
-console.log(totalDiscount);
+// let totalDiscount = totalPrice * discount;
+// console.log(totalDiscount);
 
-let ticketPrice = totalPrice - totalDiscount;
-console.log(ticketPrice);
+// let ticketPrice = totalPrice - totalDiscount;
+// console.log(ticketPrice);
 
 
-// Mostra il prezzo finale
-document.getElementById('demo').innerHTML = ("Il prezzo del tuo biglietto è di €" + totalPrice.toFixed(2));
+// // Mostra il prezzo finale
+// document.getElementById('demo').innerHTML = ("Il prezzo del tuo biglietto è di €" + totalPrice.toFixed(2));
